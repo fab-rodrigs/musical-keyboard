c33584efcae52c5faaf54ea8fc9d2944a092086d
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2024-08-17 17:11:40 -0300

    Create ADC+UART.c

---
+++ refs/ADC+UART.c
@@ -0,0 +1,131 @@
+//******************************************************************************
+//   MSP430F552x Demo - USCI_A0, 115200 UART Echo ISR, DCO SMCLK
+//
+//   Description: Echo a received character, RX ISR used. Normal mode is LPM0.
+//   USCI_A0 RX interrupt triggers TX Echo.
+//   Baud rate divider with 1048576hz = 1048576/115200 = ~9.1 (009h|01h)
+//   ACLK = REFO = ~32768Hz, MCLK = SMCLK = default DCO = 32 x ACLK = 1048576Hz
+//   See User Guide for baud rate divider table
+//
+//                 MSP430F552x
+//             -----------------
+//         /|\|                 |
+//          | |                 |
+//          --|RST              |
+//            |                 |
+//            |     P4.4/UCA1TXD|------------>
+//            |                 | 115200 - 8N1
+//            |     P4.5/UCA1RXD|<------------
+//
+//   Bhargavi Nisarga
+//   Texas Instruments Inc.
+//   April 2009
+//   Built with CCSv4 and IAR Embedded Workbench Version: 4.21
+//******************************************************************************
+
+#include <msp430.h>
+
+volatile unsigned int aux = 0;
+
+int main(void)
+{
+
+
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+    ADC12CTL0 = ADC12SHT02 + ADC12ON;         // Sampling time, ADC12 on
+    ADC12CTL1 = ADC12SHP;                     // Use sampling timer
+    ADC12IE = 0x01;                           // Enable interrupt
+    ADC12CTL0 |= ADC12ENC;
+    P6SEL |= 0x01;                            // P6.0 ADC option select
+    P1DIR |= 0x01;                            // P1.0 output
+
+    P4SEL |= BIT5+BIT4;                       // P4.4,5 = USCI_A1 TXD/RXD
+
+    UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+    UCA1CTL1 |= UCSSEL_2;                     // SMCLK
+    UCA1BR0 = 9;                              // 1MHz 115200 (see User's Guide)
+    UCA1BR1 = 0;                              // 1MHz 115200
+    UCA1MCTL |= UCBRS_1 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
+    UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+    //UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+
+    __bis_SR_register(GIE);       // Enter LPM0, interrupts enabled
+    __no_operation();                         // For debugger
+
+    while(1)
+    {
+
+        //aux++;
+        ADC12CTL0 |= ADC12SC;                   // Start sampling/conversion
+        __delay_cycles(50000);
+
+        UCA1TXBUF = aux>>8;
+        while (!(UCA1IFG&UCTXIFG));             // USCI_A1 TX buffer ready?
+        UCA1TXBUF = aux;
+        while (!(UCA1IFG&UCTXIFG));             // USCI_A1 TX buffer ready?
+
+    }
+}
+
+// Echo back RXed character, confirm TX buffer is ready first
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    switch(__even_in_range(UCA1IV,4))
+    {
+    case 0:break;                             // Vector 0 - no interrupt
+    case 2:                                   // Vector 2 - RXIFG
+        //while (!(UCA1IFG&UCTXIFG));             // USCI_A1 TX buffer ready?
+        //UCA1TXBUF = UCA1RXBUF;                  // TX -> RXed character
+        break;
+    case 4:break;                             // Vector 4 - TXIFG
+    default: break;
+    }
+}
+
+
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(ADC12_VECTOR))) ADC12_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  switch(__even_in_range(ADC12IV,34))
+  {
+  case  0: break;                           // Vector  0:  No interrupt
+  case  2: break;                           // Vector  2:  ADC overflow
+  case  4: break;                           // Vector  4:  ADC timing overflow
+  case  6:                                  // Vector  6:  ADC12IFG0
+    if (ADC12MEM0 >= 0x7ff)                 // ADC12MEM = A0 > 0.5AVcc?
+      P1OUT |= BIT0;                        // P1.0 = 1
+    else
+      P1OUT &= ~BIT0;                       // P1.0 = 0
+
+    aux = ADC12MEM0;
+  case  8: break;                           // Vector  8:  ADC12IFG1
+  case 10: break;                           // Vector 10:  ADC12IFG2
+  case 12: break;                           // Vector 12:  ADC12IFG3
+  case 14: break;                           // Vector 14:  ADC12IFG4
+  case 16: break;                           // Vector 16:  ADC12IFG5
+  case 18: break;                           // Vector 18:  ADC12IFG6
+  case 20: break;                           // Vector 20:  ADC12IFG7
+  case 22: break;                           // Vector 22:  ADC12IFG8
+  case 24: break;                           // Vector 24:  ADC12IFG9
+  case 26: break;                           // Vector 26:  ADC12IFG10
+  case 28: break;                           // Vector 28:  ADC12IFG11
+  case 30: break;                           // Vector 30:  ADC12IFG12
+  case 32: break;                           // Vector 32:  ADC12IFG13
+  case 34: break;                           // Vector 34:  ADC12IFG14
+  default: break;
+  }
+}
