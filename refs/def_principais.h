6eee948f3be708728f5d5907f02c64273c93c496
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2024-08-17 17:07:45 -0300

    Create def_principais.h

---
+++ refs/def_principais.h
@@ -0,0 +1,11 @@
+#ifndef _DEF_PRINCIPAIS_H
+#define _DEF_PRINCIPAIS_H
+
+//Defini��es de macros para o trabalho com bits
+
+#define	set_bit(y,bit)	(y|=(1<<bit))	//coloca em 1 o bit x da vari�vel Y
+#define	clr_bit(y,bit)	(y&=~(1<<bit))	//coloca em 0 o bit x da vari�vel Y
+#define cpl_bit(y,bit) 	(y^=(1<<bit))	//troca o estado l�gico do bit x da vari�vel Y
+#define tst_bit(y,bit) 	(y&(1<<bit))	//retorna 0 ou 1 conforme leitura do bit
+
+#endif
